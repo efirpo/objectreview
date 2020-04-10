09078c53b62657cc7afbf2acd9f8a722449c7c9c
Ethan Firpo <efirpo@gmail.com>
2020-04-10 15:52:15 -0700

    experiment with displaying element on checkbox being checked

--- js/scripts.js
+++ js/scripts.js
@@ -70,11 +70,19 @@ Pizza.prototype.formatOrder = function () {
   }
 }
 
+function checkedBox() {
+  var checkBox = $(".check");
+  var za = $(".za");
+  if (checkBox.checked == true) {
+    za.style.display = "block"
+  } else {
+    za.style.display = "none";
+  }
+}
 
 // UI Logic
 
 $(document).ready(function () {
-
   $("form#pizza").submit(function (event) {
     event.preventDefault();
     var pizza = new Pizza;
