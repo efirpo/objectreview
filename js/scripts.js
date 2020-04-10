0ba6c0d28721fac0d47871e4a5ae19abf61d2912
Ethan Firpo <efirpo@gmail.com>
2020-04-10 10:06:23 -0700

    build form submission UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,9 @@ function Pizza() {
   this.meats = [];
   this.toppings = [];
   this.price = 10
-}
+};
 
-Pizza.prototype.priceCalculator() {
+Pizza.prototype.priceCalculator = function () {
   if (this.size = "2") {
     this.price += 2
   }
@@ -23,10 +23,33 @@ Pizza.prototype.priceCalculator() {
   }
 
   if (this.toppings.length > 2) {
-    this.price += ((this.toppings - 2) * 1.5)
+    this.price += ((this.toppings.length - 2) * 1.5)
   }
-}
+};
 
 
 
 // UI Logic
+
+$(document).ready(function () {
+
+  $("form#pizza").submit(function (event) {
+    event.preventDefault();
+    var pizza = new Pizza;
+    pizza.size = $("#pizzasize").val();
+    $("input:checkbox[name=meatchoice]:checked").each(function () {
+      var meats = $(this).val();
+      pizza.meats.push(meats);
+    })
+    $("input:checkbox[name=toppings]:checked").each(function () {
+      var toppings = $(this).val();
+      pizza.toppings.push(toppings);
+    })
+    console.log(pizza.size);
+    console.log(pizza.meats);
+    console.log(pizza.toppings);
+    pizza.priceCalculator();
+    console.log(pizza.price);
+
+  })
+})
